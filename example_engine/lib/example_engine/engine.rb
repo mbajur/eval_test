e9c10738b05b150a766cbf94e0d6905562d42c99
Mike Bajur <mbajur@gmail.com>
2014-12-17 12:29:08 +0100

    Make it working

--- example_engine/lib/example_engine/engine.rb
+++ example_engine/lib/example_engine/engine.rb
@@ -1,5 +1,12 @@
 module ExampleEngine
   class Engine < ::Rails::Engine
     isolate_namespace ExampleEngine
+
+    config.to_prepare do
+      Dir.glob(Engine.root.join('app', 'models', '*.rb')).each do |c|
+        require_dependency(c)
+      end
+    end
+
   end
 end
